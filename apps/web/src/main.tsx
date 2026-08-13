373623c686846393ac8605c33bc0ccad87f0620b
Drug <cropflre2333@gmail.com>
2026-08-13 17:28:47 +0800

    feat: initialize theme before app render

--- apps/web/src/main.tsx
+++ apps/web/src/main.tsx
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import { initializeTheme } from './theme';
 import './styles.css';
+import './theme.css';
+
+initializeTheme();
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
